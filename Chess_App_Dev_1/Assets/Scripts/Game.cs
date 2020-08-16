40614fdc0d8c3a3eae880873f202b9f81ef44c9a
leleonak47 <leonardoinfotecnico@gmail.com>
2020-08-16 16:39:11 -0300

    Criação do jogo
    Criação do jogo

---
+++ Chess_App_Dev_1/Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject chesspiece;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //chesspiece.SetActive(true);
+        Instantiate(chesspiece, new Vector3(0, 0, -1), Quaternion.identity);
+    }
+}
